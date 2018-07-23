0094e135db477954e7ec765cf5c157f63edd8c21
root <906202066@qq.com>
2018-07-23 17:09:33 +0800

    7.55

--- DataServiceHost/Lottery.Service/Program.cs
+++ DataServiceHost/Lottery.Service/Program.cs
@@ -66,7 +66,7 @@ namespace Lottery.Service.Host
                 .UseServer(options =>
                 {
                     //  options.IpEndpoint = new IPEndPoint(IPAddress.Any, 98);  
-                    options.Port = 99;
+                    options.Port = 10099;
                     options.Ip = "127.0.0.1";
                     options.Token = "True";
                     options.ExecutionTimeoutInMilliseconds = 30000;
