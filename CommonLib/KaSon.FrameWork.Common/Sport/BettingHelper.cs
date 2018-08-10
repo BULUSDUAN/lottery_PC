dbb21ce3a62dbc010bbb6fc87fcd958341dbb2b0
Lidi <zhitian2007@126.com>
2018-08-10 11:45:06 +0800

    订单详情

--- CommonLib/KaSon.FrameWork.Common/Sport/BettingHelper.cs
+++ CommonLib/KaSon.FrameWork.Common/Sport/BettingHelper.cs
@@ -672,9 +672,9 @@ namespace KaSon.FrameWork.Common.Sport
                     switch (gameType)
                     {
                         case "T14C":
-                            return "14场胜负";
+                            return "胜负14场";
                         case "TR9":
-                            return "胜负任9";
+                            return "任选9";
                         case "T6BQC":
                             return "6场半全场";
                         case "T4CJQ":
