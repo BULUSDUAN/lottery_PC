0dda90c232eca2812e41552dfac1509c1400c3f2
Lidi <zhitian2007@126.com>
2018-08-13 14:47:16 +0800

    删除传统足球详情最后一条数据

--- ApiGateway/Lottery.Api.Controllers/OrderController.cs
+++ ApiGateway/Lottery.Api.Controllers/OrderController.cs
@@ -1458,7 +1458,7 @@ namespace Lottery.Api.Controllers
                         }
                     }
                 }
-                if (gameCode.ToUpper() == "SJB")
+                else if (gameCode.ToUpper() == "SJB")
                 {
                     Dictionary<string, object> param = new Dictionary<string, object>
                     {
