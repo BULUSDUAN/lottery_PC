65e817065d8fc08a90eb5f5983f23ba117574670
Lidi <zhitian2007@126.com>
2018-08-21 18:07:53 +0800

    六点发布

--- ApiGateway/Lottery.Api.Controllers/OrderController.cs
+++ ApiGateway/Lottery.Api.Controllers/OrderController.cs
@@ -1225,6 +1225,7 @@ namespace Lottery.Api.Controllers
             }
             param.Clear();
             param.Add("schemeId", schemeId);
+            param.Add("userToken", userToken);
             //param.Add("pageIndex", 0);
             //param.Add("pageSize", 100);
             //param.Add("UserToken", userToken);
