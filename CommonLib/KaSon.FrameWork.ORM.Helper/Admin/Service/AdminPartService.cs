6a3760abfd9fb9b60a8dabe2ec9e63808fd23fa5
刘杭州 <348846313@qq.com>
2018-10-16 16:57:22 +0800

    提交代码

---
+++ CommonLib/KaSon.FrameWork.ORM.Helper/Admin/Service/AdminPartService.cs
@@ -0,0 +1,27 @@
+﻿using EntityModel.CoreModel;
+using EntityModel.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using EntityModel;
+using Lottery.AdminApi.Model.HelpModel;
+
+namespace KaSon.FrameWork.ORM.Helper.Admin
+{
+   public class AdminPartService
+    {
+        public A20131105CouponsInfoCollection QueryCouponsList(string summary, bool? canUsable, string belongUserId, int pageIndex, int pageSize, string userToken)
+        {
+            var result = new A20131105CouponsInfoCollection();
+            var totalCount = 0;
+            var activityManager = new A20131105Manager();
+            var list = activityManager.QueryCouponsList(summary, canUsable, belongUserId, pageIndex, pageSize, out totalCount);
+            result.TotalCount = totalCount;
+            result.List.AddRange(list);
+
+            return result;
+            return new A20131105CouponsInfoCollection();
+        }
+    }
+}
