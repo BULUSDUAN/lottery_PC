41dd73ab7f4ffbd7fae24982e76cad9d0f2e5e61
hrenjun <497189599@qq.com>
2018-11-19 17:10:39 +0800

    历史战绩

--- CommonLib/KaSon.FrameWork.ORM.Helper/Redis/WebRedisHelper.cs
+++ CommonLib/KaSon.FrameWork.ORM.Helper/Redis/WebRedisHelper.cs
@@ -9,6 +9,8 @@ using KaSon.FrameWork.Common.Redis;
 using KaSon.FrameWork.Common;
 using static EntityModel.CoreModel.ReportInfo;
 using EntityModel.Enum;
+using EntityModel;
+using KaSon.FrameWork.Common.JSON;
 
 namespace KaSon.FrameWork.ORM.Helper
 {
@@ -427,5 +429,394 @@ namespace KaSon.FrameWork.ORM.Helper
             cache.ReportItemList = query.Skip(pageIndex * pageSize).Take(pageSize).ToList();
             return cache;
         }
+        #region 查询用户个人信息
+        public static ProfileUserInfo QueryProfileUserInfo(string userId)
+        {
+            var info = LoadProfileUserInfoFromRedis(userId);
+            if (info == null)
+            {
+                var biz = new CacheDataBusiness();
+                info = biz.QueryProfileUserInfo(userId);
+                SaveProfileUserInfoToRedis(userId, info);
+            }
+            return info;
+        }
+
+        private static ProfileUserInfo LoadProfileUserInfoFromRedis(string userId)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+
+            var key = string.Format("ProfileUserInfo_{0}", userId);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<ProfileUserInfo>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveProfileUserInfoToRedis(string userId, ProfileUserInfo info)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+                var key = string.Format("ProfileUserInfo_{0}", userId);
+                int validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        #endregion
+        #region 查询中奖等级
+
+        public static ProfileBonusLevelInfo QueryProfileBonusLevelInfo(string userId)
+        {
+            var info = LoadProfileBonusLevelInfoFromRedis(userId);
+            if (info == null)
+            {
+                var biz = new CacheDataBusiness();
+                info = biz.QueryProfileBonusLevelInfo(userId);
+                SaveProfileBonusLevelInfoToRedis(userId, info);
+            }
+            return info;
+        }
+
+        private static ProfileBonusLevelInfo LoadProfileBonusLevelInfoFromRedis(string userId)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("ProfileBonusLevelInfo_{0}", userId);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<ProfileBonusLevelInfo>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveProfileBonusLevelInfoToRedis(string userId, ProfileBonusLevelInfo info)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("ProfileBonusLevelInfo_{0}", userId);
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region 查询最新中奖
+
+        public static ProfileLastBonusCollection QueryProfileLastBonusCollection(string userId)
+        {
+            var info = LoadProfileLastBonusCollectionFromRedis(userId);
+            if (info == null)
+            {
+                var biz = new CacheDataBusiness();
+                info = biz.QueryProfileLastBonusCollection(userId);
+                SaveProfileLastBonusCollectionToRedis(userId, info);
+            }
+            return info;
+        }
+
+        private static ProfileLastBonusCollection LoadProfileLastBonusCollectionFromRedis(string userId)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("ProfileLastBonusCollection_{0}", userId);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<ProfileLastBonusCollection>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveProfileLastBonusCollectionToRedis(string userId, ProfileLastBonusCollection info)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("ProfileLastBonusCollection_{0}", userId);
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region 查询个人数据
+
+        public static ProfileDataReport QueryProfileDataReport(string userId)
+        {
+            var info = LoadProfileDataReportFromRedis(userId);
+            if (info == null)
+            {
+                var biz = new CacheDataBusiness();
+                info = biz.QueryProfileDataReport(userId);
+                SaveProfileDataReportToRedis(userId, info);
+            }
+            return info;
+        }
+
+        private static ProfileDataReport LoadProfileDataReportFromRedis(string userId)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("ProfileDataReport_{0}", userId);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<ProfileDataReport>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveProfileDataReportToRedis(string userId, ProfileDataReport info)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("ProfileDataReport_{0}", userId);
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+            #region 查询用户跟单人数
+
+        public static int QueryTogetherFollowerCount(string userId)
+        {
+            var info = LoadTogetherFollowerCountFromRedis(userId);
+            if (info == -1)
+            {
+
+                info = new SqlQueryBusiness().QueryTogetherFollowerCount(userId);
+                SaveTogetherFollowerCountToRedis(userId, info);
+            }
+            return info;
+        }
+
+        private static int LoadTogetherFollowerCountFromRedis(string userId)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("FollowerCount_{0}", userId);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return -1;
+            try
+            {
+                return int.Parse(v.ToString());
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        private static void SaveTogetherFollowerCountToRedis(string userId, int count)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("FollowerCount_{0}", userId);
+            try
+            {
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, count.ToString(), Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region 查询用户战绩
+
+        public static UserBeedingListInfoCollection QueryUserBeedingListInfoCollection(string userId)
+        {
+            var info = LoadUserBeedingListInfoCollectionFromRedis(userId);
+            if (info == null)
+            {
+              
+                info = new Sports_Business().QueryUserBeedingList("", "", userId, string.Empty, 0, 100, QueryUserBeedingListOrderByProperty.TotalBonusMoney, OrderByCategory.DESC);
+                SaveUserBeedingListInfoCollectionToRedis(userId, info);
+            }
+            return info;
+        }
+
+        private static UserBeedingListInfoCollection LoadUserBeedingListInfoCollectionFromRedis(string userId)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("UserBeedingList_{0}", userId);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<UserBeedingListInfoCollection>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveUserBeedingListInfoCollectionToRedis(string userId, UserBeedingListInfoCollection info)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("UserBeedingList_{0}", userId);
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region 查询用户当前的订单
+
+        public static UserCurrentOrderInfoCollection QueryUserCurrentOrderInfoCollection(string userId, string gameCode)
+        {
+            var info = LoadUserCurrentOrderInfoCollectionFromRedis(userId, gameCode);
+            if (info == null)
+            {
+               
+                info = new Sports_Business().QueryUserCurrentOrderList(userId, gameCode, 0, 30);
+                SaveUserCurrentOrderInfoCollectionToRedis(userId, gameCode, info);
+            }
+            return info;
+        }
+
+        private static UserCurrentOrderInfoCollection LoadUserCurrentOrderInfoCollectionFromRedis(string userId, string gameCode)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("UserCurrentOrderInfoCollection_{0}_{1}", userId, gameCode);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<UserCurrentOrderInfoCollection>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveUserCurrentOrderInfoCollectionToRedis(string userId, string gameCode, UserCurrentOrderInfoCollection info)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("UserCurrentOrderInfoCollection_{0}_{1}", userId, gameCode);
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region 查询用户中奖订单
+
+        public static BonusOrderInfoCollection QueryBonusOrderInfoCollection(string userId, string gameCode, string gameType)
+        {
+            gameCode = (gameCode == "SZC" ? gameType : gameCode);
+            gameType = (gameCode == "SZC" ? "" : gameType);
+            var info = LoadBonusOrderInfoCollectionFromRedis(userId, gameCode, gameType);
+            if (info == null)
+            {
+                info =new DataQuery().QueryBonusInfoList(userId, gameCode, gameType, string.Empty, string.Empty, string.Empty, 0, 30);
+                SaveBonusOrderInfoCollectionToRedis(userId, gameCode, gameType, info);
+            }
+            return info;
+        }
+
+        private static BonusOrderInfoCollection LoadBonusOrderInfoCollectionFromRedis(string userId, string gameCode, string gameType)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("BonusOrderInfoCollection_{0}_{1}_{2}", userId, gameCode, gameType);
+            var v = db.ExistsAsync(key).Result;
+            if (!v)
+                return null;
+            try
+            {
+                return JsonSerializer.Deserialize<BonusOrderInfoCollection>(v.ToString());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static void SaveBonusOrderInfoCollectionToRedis(string userId, string gameCode, string gameType, BonusOrderInfoCollection info)
+        {
+            var db = RedisHelperEx.DB_UserBlogData;//.DB_UserBlogData;
+            var key = string.Format("BonusOrderInfoCollection_{0}_{1}_{2}", userId, gameCode, gameType);
+            try
+            {
+                var json = JsonSerializer.Serialize(info);
+                var validMinute = int.Parse(ConfigHelper.AllConfigInfo["BlogValidMinute"].ToString());
+                db.Set(key, json, Convert.ToInt32(TimeSpan.FromMinutes(validMinute)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
     }
 }
