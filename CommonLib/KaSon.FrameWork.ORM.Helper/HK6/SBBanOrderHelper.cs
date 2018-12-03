da9526d9774227a72199109b1f493a02e7a7ff2a
kason <asus@DESKTOP-N0SLHVM>
2018-12-03 15:05:23 +0800

    11

---
+++ CommonLib/KaSon.FrameWork.ORM.Helper/HK6/SBBanOrderHelper.cs
@@ -0,0 +1,177 @@
+﻿using EntityModel;
+using KaSon.FrameWork.Common.Hk6;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace KaSon.FrameWork.ORM.Helper
+{
+    /// <summary>
+    /// 半色
+    /// </summary>
+   public class SBBanOrderHelper : IOrderHelper
+    {
+        private IDbProvider DB = null;
+       
+        public SBBanOrderHelper(IDbProvider _DB) {
+            DB = _DB;
+        }
+        public void WinMoney(blast_bet_orderdetail orderdetail, string winNum) {
+            string tm = winNum.Split('|')[1];
+            string zm = winNum.Split('|')[0];
+            var codeArr = orderdetail.AnteCodes.Trim().Split(',');
+            int userId = orderdetail.userId;
+            int winCount = 0;
+            List<string> winCodeList = new List<string>();
+
+            string antuCode = orderdetail.AnteCodes;
+           bool isWin = false;
+            switch (antuCode)
+            {
+                #region 红
+                case "RedDan":
+                    if (SXHelper.RedBox.Contains(tm) && int.Parse(tm)%2!=0)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "RedSua":
+                    if (SXHelper.RedBox.Contains(tm) && int.Parse(tm) % 2 == 0)
+                    {
+                        isWin = true;
+                    }
+                    break;
+                case "RedXiao":
+                    if (SXHelper.RedBox.Contains(tm) && int.Parse(tm) <=24)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "RedDa":
+                    if (SXHelper.RedBox.Contains(tm) && int.Parse(tm) > 24)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                #endregion
+                #region 蓝
+                case "BluDan":
+                    if (SXHelper.BluBox.Contains(tm) && int.Parse(tm) % 2 != 0)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "BluSua":
+                    if (SXHelper.BluBox.Contains(tm) && int.Parse(tm) % 2 == 0)
+                    {
+                        isWin = true;
+                    }
+                    break;
+                case "BluXiao":
+                    if (SXHelper.BluBox.Contains(tm) && int.Parse(tm) <= 24)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "BluDa":
+                    if (SXHelper.BluBox.Contains(tm) && int.Parse(tm) > 24)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                #endregion
+                #region 绿
+                case "GreenDan":
+                    if (SXHelper.GreenBox.Contains(tm) && int.Parse(tm) % 2 != 0)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "GreenSua":
+                    if (SXHelper.GreenBox.Contains(tm) && int.Parse(tm) % 2 == 0)
+                    {
+                        isWin = true;
+                    }
+                    break;
+                case "GreenXiao":
+                    if (SXHelper.GreenBox.Contains(tm) && int.Parse(tm) <= 24)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "GreenDa":
+                    if (SXHelper.GreenBox.Contains(tm) && int.Parse(tm) > 24)
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                #endregion
+                default:
+
+                    break;
+            }
+            if (int.Parse(tm)==49)
+            {
+                isWin = false;
+                //是否和局
+            }
+            //故中奖
+            //计算中奖号码
+            decimal Odds = decimal.Parse(orderdetail.OddsArr);
+
+            decimal winMoney = orderdetail.unitPrice * (Odds);
+
+            int orderDetailId = orderdetail.id;
+
+            string windesc = string.Join(",", winCodeList);
+            if (isWin)
+            {
+
+                DB.GetDal<blast_bet_orderdetail>().Update(b => new blast_bet_orderdetail
+                {
+                    winNumber = winNum,
+                    BonusAwardsMoney = winMoney,
+                    updateTime = DateTime.Now,
+                    BonusStatus = 2,  //为中奖状态
+                    winNumberDesc= ""
+
+                }, b => b.id == orderDetailId);
+
+            }
+            else {
+
+                DB.GetDal<blast_bet_orderdetail>().Update(b => new blast_bet_orderdetail
+                {
+                    //winNumber = winNum,
+                    //BonusAwardsMoney = winMoney,
+                    updateTime = DateTime.Now,
+                    BonusStatus =3, //为中奖状态
+                    winNumber= winNum,
+                    winNumberDesc = windesc
+                }, b => b.id == orderDetailId);
+            }
+
+
+
+            //添加用户金币 加钱  blast_lhc_member
+            if (isWin)
+            {
+                DB.GetDal<blast_lhc_member>().Update(b => new blast_lhc_member
+                {
+                    gameMoney = b.gameMoney + winMoney
+                }, b => b.userId== userId);
+            }
+              
+
+        }
+    }
+}
---
+++ CommonLib/KaSon.FrameWork.ORM.Helper/HK6/SBSanSeOrderHelper.cs
@@ -0,0 +1,106 @@
+﻿using EntityModel;
+using KaSon.FrameWork.Common.Hk6;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace KaSon.FrameWork.ORM.Helper
+{
+    /// <summary>
+    /// 三色
+    /// </summary>
+   public class SBSanSeOrderHelper : IOrderHelper
+    {
+        private IDbProvider DB = null;
+       
+        public SBSanSeOrderHelper(IDbProvider _DB) {
+            DB = _DB;
+        }
+        public void WinMoney(blast_bet_orderdetail orderdetail, string winNum) {
+            string tm = winNum.Split('|')[1];
+            string zm = winNum.Split('|')[0];
+            var codeArr = orderdetail.AnteCodes.Trim().Split(',');
+            int userId = orderdetail.userId;
+            int winCount = 0;
+            List<string> winCodeList = new List<string>();
+
+            string antuCode = orderdetail.AnteCodes;
+           bool isWin = false;
+            switch (antuCode)
+            {
+                case "Red":
+                    if (SXHelper.GreenBox.Contains(tm))
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "Blu":
+                    if (SXHelper.BluBox.Contains(tm))
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                case "Green":
+                    if (SXHelper.GreenBox.Contains(tm))
+                    {
+                        isWin = true;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+            //故中奖
+            //计算中奖号码
+            decimal Odds = decimal.Parse(orderdetail.OddsArr);
+
+            decimal winMoney = orderdetail.unitPrice * (Odds);
+
+            int orderDetailId = orderdetail.id;
+
+            string windesc = string.Join(",", winCodeList);
+            if (isWin)
+            {
+
+                DB.GetDal<blast_bet_orderdetail>().Update(b => new blast_bet_orderdetail
+                {
+                    winNumber = winNum,
+                    BonusAwardsMoney = winMoney,
+                    updateTime = DateTime.Now,
+                    BonusStatus = 2,  //为中奖状态
+                    winNumberDesc= ""
+
+                }, b => b.id == orderDetailId);
+
+            }
+            else {
+
+                DB.GetDal<blast_bet_orderdetail>().Update(b => new blast_bet_orderdetail
+                {
+                    //winNumber = winNum,
+                    //BonusAwardsMoney = winMoney,
+                    updateTime = DateTime.Now,
+                    BonusStatus =3, //为中奖状态
+                    winNumber= winNum,
+                    winNumberDesc = windesc
+                }, b => b.id == orderDetailId);
+            }
+
+
+
+            //添加用户金币 加钱  blast_lhc_member
+            if (isWin)
+            {
+                DB.GetDal<blast_lhc_member>().Update(b => new blast_lhc_member
+                {
+                    gameMoney = b.gameMoney + winMoney
+                }, b => b.userId== userId);
+            }
+              
+
+        }
+    }
+}
