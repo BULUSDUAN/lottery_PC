5087ba488a792d2d855c3dc8234021a054206976
kason <asus@DESKTOP-N0SLHVM>
2018-12-21 13:55:40 +0800

    六合彩

--- CommonLib/KaSon.FrameWork.ORM.Helper/HK6/DataServiceHelper.cs
+++ CommonLib/KaSon.FrameWork.ORM.Helper/HK6/DataServiceHelper.cs
@@ -30,9 +30,64 @@ namespace KaSon.FrameWork.ORM.Helper
                       where b.actionTime >= date
                       orderby b.actionTime ascending
                       select b).FirstOrDefault();
-
+            string num = (mb.actionNo + "").Replace(DateTime.Now.Year.ToString(), "");
+            mb.PreData = GetWinData(mb);
             return mb;
 
+        }
+        private blast_data GetWinData(blast_data_time btime) {
+            int issueNo = btime.actionNo;
+            if (issueNo<2000)
+            {
+                issueNo = int.Parse(btime.actionTime.Year + "" + issueNo);
+            }
+            var query = DB.CreateQuery<blast_data>();
+            blast_data data = new blast_data();
+            
+                int year = int.Parse((issueNo + "").Substring(0, 4));
+                int iNo = int.Parse((issueNo + "").Substring(4));
+                if (iNo - 1 <= 0)
+                {
+                    string date = (year - 1) + "-12-01";
+                    string ete = (year) + "-01-01";
+                    DateTime sdate = DateTime.Parse(date);
+                    DateTime edate = DateTime.Parse(ete);
+                    //上一年的期号
+                    data = (from b in query
+                            where b.kjtime > sdate && b.kjtime < edate
+                            orderby b.kjtime descending
+                            select b).FirstOrDefault();
+                    if (data == null)
+                    {
+                        blast_data_time datatime = (from b in DB.CreateQuery<blast_data_time>()
+                                where b.actionTime > sdate && b.actionTime < edate
+                                orderby b.actionTime descending
+                                select b).FirstOrDefault();
+                        data=   new blast_data() { issueNo = datatime.actionNo, kjdata = "", typeid = 1, kjnumber = "", kjtime = datatime.actionTime };
+                    }
+                }
+                else {
+                    int nissueNo = issueNo - 1;
+                    data = query.Where(b => b.issueNo == nissueNo).FirstOrDefault();
+                    if (data == null)
+                    {
+                        blast_data_time datatime = DB.CreateQuery<blast_data_time>().Where(b => b.actionNo == nissueNo).FirstOrDefault();
+                        data = new blast_data() { issueNo = datatime.actionNo, kjdata = "", typeid = 1, kjnumber = "", kjtime = datatime.actionTime };
+                    }
+                }
+               
+            
+            string str = data.kjdata + "";
+
+
+            if (str.Contains(",")&& !str.Contains(","))
+            {
+                int index = str.LastIndexOf(',');
+                string temp = str.Substring(0, index);
+                string temp1 = str.Substring(index);
+                data.kjdata = temp + "+" + temp1.Replace(",", "");
+            }
+            return data;
         }
        
     }
---
+++ HK6ServiceHost/PK.Service.ModuleServices/Repository.cs
@@ -0,0 +1,26 @@
+﻿using Kason.Sg.Core.CPlatform.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Kason.Sg.Core.Nlog;
+using KaSon.FrameWork.ORM;
+
+namespace PK.Service.ModuleServices
+{
+    public class Repository : BaseRepository
+    {
+        ILogger<Repository> _Log;
+
+        internal IDbProvider DB = null;
+        internal IDbProvider LDB = null;
+        //  public NLogger UserNLog;
+        public Repository(ILogger<Repository> log, IDbProvider _DB, IDbProvider _LDB) {
+            _Log = log;
+            LDB = _LDB;
+            DB = _DB;
+            // UserNLog = (NLogger)_LogFactory.CreateLogger("User");
+            //UserNLog = (NLogger)_LogFactory.CreateLogger("User");
+        }
+    }
+}
